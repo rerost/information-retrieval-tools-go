b03ea2eed8d36368946c4e72106f6a5ddc2b57d6
rerost <gmailii@yahoo.co.jp>
2019-03-23 21:09:50 +0900

    strange

--- interleaving/interleaving_bench_test.go
+++ interleaving/interleaving_bench_test.go
@@ -1,56 +1,29 @@
 package interleaving_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/rerost/information-retrieval-tools-go/interleaving"
 )
 
-// func BenchmarkInterleaving(b *testing.B) {
-// 	for _, inOutPair := range inOutPairs {
-// 		inOutPair := inOutPair
-// 		if inOutPair.Out.Error != nil {
-// 			continue
-// 		}
-// 		b.Run(inOutPair.Name, func(b *testing.B) {
-// 			b.ResetTimer()
-// 			for i := 0; i < b.N; i++ {
-// 				i, err := interleaving.NewInterleaving(inOutPair.In.PrioritizeRanking, inOutPair.In.RankingA, inOutPair.In.RankingB)
-// 				if err != nil {
-// 					b.Error(err)
-// 					return
-// 				}
-// 				_ = i.Perform()
-// 			}
-// 		})
-// 	}
-// }
-type DummyPrioritizeRanking struct {
-	isA bool
-}
-
-func NewDummyPrioritizeRanking(isA bool) interleaving.PrioritizeRanking {
-	return DummyPrioritizeRanking{isA}
-}
-func (d DummyPrioritizeRanking) IsA() bool {
-	return d.isA
-}
-
-type DummyItem int
-
-func (e DummyItem) Key() string {
-	return fmt.Sprintf("%v", e)
-}
-
-func ToRanking(arr []int) interleaving.Ranking {
-	result := make(interleaving.Ranking, len(arr), len(arr))
-
-	for i, elem := range arr {
-		result[i] = DummyItem(elem)
+func BenchmarkInterleaving(b *testing.B) {
+	for _, inOutPair := range inOutPairs {
+		inOutPair := inOutPair
+		if inOutPair.Out.Error != nil {
+			continue
+		}
+		b.Run(inOutPair.Name, func(b *testing.B) {
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				i, err := interleaving.NewInterleaving(inOutPair.In.PrioritizeRanking, inOutPair.In.RankingA, inOutPair.In.RankingB)
+				if err != nil {
+					b.Error(err)
+					return
+				}
+				_ = i.Perform()
+			}
+		})
 	}
-
-	return result
 }
 
 func CreateDummyArr(size int, weight int) []int {
